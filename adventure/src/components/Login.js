39a55ad675a5e3d0f3d9a4c0495ac77b6e2c0588
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2019-01-22 14:48:34 -0500

    preparing to deploy

--- adventure/src/components/Login.js
+++ adventure/src/components/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import { Input, Form, FormGroup } from 'reactstrap';
+import { Form } from 'reactstrap';
 
 
 class Login extends React.Component {
