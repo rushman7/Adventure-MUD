a2fe9b86c68f0b20659d363d281b61d84eac5629
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2019-01-22 16:31:58 -0500

    changing default path

--- adventure/src/App.js
+++ adventure/src/App.js
@@ -9,8 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route exact path='/' component={Home} />
-        <Route path={process.env.PUBLIC_URL} components={App}></Route>
+        <Route exact path='' component={Home} />
         <Route path='/api/registration/' component={Register} />
         <Route path='/api/login/' component={Login} />
         <Route path='/api/gamestart' component={GameStart} />
