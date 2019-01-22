c12ab1dedfa5c53cf86944f88d72187c87826325
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2019-01-22 16:24:09 -0500

    fixed url

--- adventure/src/App.js
+++ adventure/src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Route } from 'react-router-dom'
+import { Route, Link } from 'react-router-dom'
 import Register from './components/Register'
 import Login from './components/Login'
 import GameStart from './components/GameStart'
-import Home from './components/Home'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Route exact path='/' component={Home} />
+        <Route path={process.env.PUBLIC_URL} components={App}></Route>
         <Route path='/api/registration/' component={Register} />
         <Route path='/api/login/' component={Login} />
         <Route path='/api/gamestart' component={GameStart} />
