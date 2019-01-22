ec244cd165e7b673e873352bc30df87d09841359
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2019-01-22 16:35:33 -0500

    home import

--- adventure/src/App.js
+++ adventure/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Route, Link } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import Register from './components/Register'
 import Login from './components/Login'
 import GameStart from './components/GameStart'
+import Home from './components/Home'
 
 class App extends Component {
   render() {
